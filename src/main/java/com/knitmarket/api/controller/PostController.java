533be69d45b3ae46b6407ffd52e4d48635a0e945
HAN-SEOHYUN <000312>
2022-07-22 16:07:30 +0900

    json 데이터 검증하기

--- src/main/java/com/knitmarket/api/controller/PostController.java
+++ src/main/java/com/knitmarket/api/controller/PostController.java
@@ -3,9 +3,15 @@ package com.knitmarket.api.controller;
 import com.knitmarket.api.request.PostCreate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.awt.image.ImageProducer;
+import java.sql.ClientInfoStatus;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -14,10 +20,20 @@ public class PostController {
     //
 
     @PostMapping("/posts")
-    public String post(@RequestBody @Valid PostCreate params, BindingResult result){
+    public Map<String,String> post(@RequestBody @Valid PostCreate params, BindingResult result){
 
-        log.info("params={}",params.toString());
-        return "HELLO WORLD";
+        if(result.hasErrors()){
+            List<FieldError> fieldErrors = result.getFieldErrors();
+            FieldError firstFieldError = fieldErrors.get(0);
+            String fieldName = firstFieldError.getField(); //title
+            String errorMessage = firstFieldError.getDefaultMessage(); //..에러메세지
+
+            Map<String,String> error = new HashMap<>();
+            error.put(fieldName,errorMessage);
+            return error;
+
+        }
+        return Map.of();
     }
 
 }

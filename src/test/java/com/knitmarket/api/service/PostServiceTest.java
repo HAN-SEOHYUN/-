11df291d519771b15764ea9f98f678966034167a
HAN-SEOHYUN <000312>
2022-07-24 20:58:22 +0900

    단건조회

--- src/test/java/com/knitmarket/api/service/PostServiceTest.java
+++ src/test/java/com/knitmarket/api/service/PostServiceTest.java
@@ -1,24 +1,39 @@
 package com.knitmarket.api.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
 import com.knitmarket.api.domain.Post;
 import com.knitmarket.api.repository.PostRepository;
 import com.knitmarket.api.request.PostCreate;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class PostServiceTest {
+
 
     @Autowired
     private  PostService postService;
 
     @Autowired
     private PostRepository postRepository;
+
+    @BeforeEach
+    void clean(){
+        postRepository.deleteAll();
+    }
 
 
     @Test
@@ -46,16 +61,23 @@ class PostServiceTest {
     @DisplayName("글 1개 조회")
     void test2(){
         //given
-        Long postId = 1L;
+        Post requestPost = Post.builder()
+                .title("foo")
+                .content("bar")
+                .build();
+
+        postRepository.save(requestPost);
 
         //when
-         Post post= postService.get(postId);
+         Post post= postService.get(requestPost.getId());
 
          //then
         Assertions.assertNotNull(post);
-
-
+        assertEquals("foo",post.getTitle());
+        assertEquals("bar",post.getContent());
     }
+
+
 
 
 
